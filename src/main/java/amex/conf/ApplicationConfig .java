b9f5744da07974df42702687ea146b8ae5fefa04
nehamunugala <nehamunugala@gmail.com>
2018-09-24 12:28:56 -0400

    added config

---
+++ src/main/java/amex/conf/ApplicationConfig .java
@@ -0,0 +1,20 @@
+package amex.code.test.conf;
+
+import amex.code.test.ws.RESTAPITest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> set = new HashSet<Class<?>>();
+        set.add(RESTAPITest.class);
+        return set;
+    }
+
+}
